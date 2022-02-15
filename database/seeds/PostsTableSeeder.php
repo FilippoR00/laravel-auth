3e9612db1284c6effcb9b09079909e5dda95b4bb
Filippo Rossi <rossifilippo2000@gmail.com>
2022-02-15 16:07:07 +0100

    seeding post + init crud (index)

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Post;
+use Illuminate\Support\Str;
 
 class PostsTableSeeder extends Seeder
 {
@@ -9,8 +12,17 @@ class PostsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i < 10 ; $i++) { 
+            $newPost = new Post();
+
+            $newPost->title = $faker->words(4, true);
+            $newPost->slug = Str::of($newPost->title)->slug("-");
+            $newPost->content = $faker->text(200);
+            $newPost->published = rand(0,1);
+
+            $newPost->save();
+        }
     }
 }
